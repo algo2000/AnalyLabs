b60ca3b57b2123067382b3ad21963a361fc7d6a2
leo <algo2000@naver.com>
2021-11-10 23:44:13 +0900

    add Plotly React 테스트 컴포넌트 작성

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import { Card } from "@mui/material";
+import styled from "styled-components";
+
+const DTestPlot = dynamic(import("../components/common/plotly/TestPlot"), {
+  ssr: false,
+});
+
+const BlockCard = styled(Card)`
+  display: inline-block;
+`;
 
 function Index() {
-  return <></>;
+  return (
+    <>
+      <BlockCard>
+        <DTestPlot />
+      </BlockCard>
+    </>
+  );
 }
 
 export default Index;
