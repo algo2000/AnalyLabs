1f100aa5776ab512f31516812ba19f1ae261b6c0
leo <algo2000@naver.com>
2021-11-11 10:18:36 +0900

    del

--- renderer/pages/next/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Next() {
-  return <></>;
-}
-
-export default Next;
--- renderer/theme/theme.ts
+++
@@ -1,7 +0,0 @@
-import lightTheme from "./lightTheme";
-
-export const theme = {
-  lightTheme,
-};
-
-export default theme;
